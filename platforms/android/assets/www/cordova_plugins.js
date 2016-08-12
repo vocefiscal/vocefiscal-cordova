14e876ecd310a72e37abf348291a28d5aa4c0787
LuizNach <luiz.nach@gmail.com>
2016-08-12 16:45:19 -0300

    First commit for the App
    Base on Chris Ward Cordova-QR-Code-Reader

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,51 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-whitelist.whitelist",
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "id": "org.apache.cordova.dialogs.notification",
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "pluginId": "org.apache.cordova.dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "pluginId": "org.apache.cordova.dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "pluginId": "org.apache.cordova.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.inappbrowser": "0.6.0",
+    "phonegap-plugin-barcodescanner": "4.0.2"
+};
+// BOTTOM OF METADATA
+});
